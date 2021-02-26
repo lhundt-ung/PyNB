5bf5cc504906ad32b59e973a8cdaa71461c6f382
Lance Hundt <lance.hundt@ung.edu>
2021-02-26 13:25:05 -0500

    fix

--- _build/jupyter_execute/sb/0.py
+++
@@ -1,3 +0,0 @@
-# Sandboxes
-
-A sandbox is digital playground, an environment created for experimentation and fun. I'm creating sandbox content for **Intro to Applied Statistics** using Python's `datascience` package. The notebooks in this section are intended to make class examples using my own datasets easily accessible to my students.
